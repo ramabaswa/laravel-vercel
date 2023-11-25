474f0af621e62eb94ba0f03b93fb7035261a3524
Ram Baswa <rama.baswa@gmail.com>
2023-11-24 21:06:42 -0500

    made change to api function

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 use App\Http\Controllers\Simple;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::get('/data', [Simple::class, 'index']);
